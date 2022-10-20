e4e8300448da41609d0bc47f6f6ffec9a27ec086
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2022-10-20 16:56:50 -0300

    Arquivos curso

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import FAQScreen from "../src/screens/FAQScreen";
+
+export default FAQScreen
+
+export async function getStaticProps() {
+  const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(FAQ_API_URL).then(res => res.json());
+
+  return {
+    props: { faq }, // will be passed to the page component as props
+  }
+}
